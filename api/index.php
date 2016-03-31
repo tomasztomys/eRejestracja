d9140f3732dd0bb85a923947b0ae9d51c228f09d
Tomasz Tomys <tomasz@tomys.pl>
2016-03-31 03:22:39 +0200

    Add first function of recognize method request.

--- api/index.php
+++ api/index.php
@@ -1,2 +1,29 @@
 <?php
-phpinfo();
+
+function getRequest() {
+    echo 'I\'m get request';
+}
+function postRequest() {
+    echo 'I\'m post request';
+}
+function deleteRequest() {
+    echo 'I\'m delete request';
+}
+function patchRequest() {
+    echo 'I\'m patch request';
+}
+
+switch($_SERVER['REQUEST_METHOD']) {
+    case 'GET':
+        getRequest();
+        break;
+    case 'POST':
+        postRequest();
+        break;
+    case 'DELETE':
+        deleteRequest();
+        break;
+    case 'PATCH':
+        patchRequest();
+        break;
+}
