5bff24b95bb455ceeeb1033d2ca46df87e7720a1
Dariusz Paluch <dariusz.paluch@hotmail.com>
2016-04-11 21:51:18 +0200

    Refactor App Routes and index to pass eslint

--- front/src/scripts/components/Routes.jsx
+++ front/src/scripts/components/Routes.jsx
@@ -1,34 +1,43 @@
-import React from 'react'
+import React from 'react';
 import {
   Router,
   Route,
-  IndexRoute,
   Redirect,
   browserHistory
-} from 'react-router'
-import store from 'store'
-import Paths from '../constants/PathsConstants'
+} from 'react-router';
+import Paths from '../constants/PathsConstants';
 
-import App from './App'
+import App from './App';
 
-import { Login } from './views/login'
-import { Navigation } from './views/navigation'
+import { Login } from './views/login';
+import { Navigation } from './views/navigation';
 
 export default class Routes extends React.Component {
-
-  constructor(props) {
-    super(props);
-  }
 
   render() {
     return (
       <Router history={ browserHistory }>
-        <Redirect from={ Paths.root } to={ Paths.navigation } />
-        <Route path={ Paths.root } component={ App }>
-          <Route path={ Paths.navigation } component={ Navigation } />
-          <Route path={ Paths.login } component={ Login } />
+        <Redirect
+          from={ Paths.root }
+          to={ Paths.navigation }
+        />
+        <Route
+          path={ Paths.root }
+          component={ App }
+        >
+          <Route
+            path={ Paths.navigation }
+            component={ Navigation }
+          />
+          <Route
+            path={ Paths.login }
+            component={ Login }
+          />
         </Route>
-        <Redirect from={ Paths.notFound } to={ Paths.root } />
+        <Redirect
+          from={ Paths.notFound }
+          to={ Paths.root }
+        />
       </Router>
     );
   }
