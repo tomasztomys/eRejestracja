a290a0ff6ba12e87cc1845f09040a0e0bb43ab0a
Tomasz Tomys <tomasz@tomys.pl>
2016-04-17 23:56:15 +0200

    Fix doctors' call. Add CORS.

--- api/index.php
+++ api/index.php
@@ -19,9 +19,13 @@ $appConfig = new \Configs\Config();
 
 $app->add(function (Request $request, Response $response, callable $next) {
 
-    $newResponse = $response->withHeader('Content-type', 'application/json');
+    $response = $response->withHeader('Access-Control-Allow-Origin', '*');
+    $response = $response->withHeader('Access-Control-Allow-Credentials', 'true');
+    $response = $response->withHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type, Content-Range, Content-Disposition, Content-Description');
+    $response = $response->withHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    $response = $response->withHeader('Content-Type', 'application/json');
 
-    return $next($request, $newResponse);
+    return $next($request, $response);
 });
 
 
