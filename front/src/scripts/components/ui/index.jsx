447e49f0de9395a9ce544e3ccbc0b07db4ce93e5
Dariusz Paluch <dariusz.paluch@hotmail.com>
2016-04-19 16:05:02 +0200

    Add table component to ui

--- front/src/scripts/components/ui/index.jsx
+++ front/src/scripts/components/ui/index.jsx
@@ -1,10 +1,12 @@
 import { Input } from './input';
 import { List, ListItem } from './list';
 import { Button } from './button';
+import { Table } from './table';
 
 export {
   Input,
   List,
   ListItem,
-  Button
+  Button,
+  Table
 };
