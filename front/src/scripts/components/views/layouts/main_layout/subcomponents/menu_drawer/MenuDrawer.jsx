ea9d07617a0979f1e576ec5c60396b052013ecb0
Dariusz Paluch <dariusz.paluch@hotmail.com>
2016-04-24 23:03:21 +0200

    Add open subMenu function to drawer

--- front/src/scripts/components/views/layouts/main_layout/subcomponents/menu_drawer/MenuDrawer.jsx
+++ front/src/scripts/components/views/layouts/main_layout/subcomponents/menu_drawer/MenuDrawer.jsx
@@ -10,12 +10,26 @@ import style from './menu_drawer';
 import AdminNavigationLinks from '../../../../../../constants/AdminNavigationLinks';
 
 export default class MenuDrawer extends Component {
+  constructor() {
+    super();
+    let menuItems = AdminNavigationLinks.map((menuItem, index) => {
+      let item = menuItem;
+
+      item.open = false;
+      item.id = index;
+      return item;
+    });
+
+    this.state = {
+      menuItems
+    };
+  }
 
   _renderMenuItem(item) {
     return (
-      <List className={ style['main-item'] }>
+      <List>
         <ListItem
-          className={ style['menu-item'] }
+          className={ style['main-item'] }
           key={ item.label }
           caption={ item.label }
           onClick={ () => {} }
@@ -35,6 +49,15 @@ export default class MenuDrawer extends Component {
     );
   }
 
+  _toggleMenu(id) {
+    let { menuItems } = this.state;
+
+    menuItems[id].open = !menuItems[id].open;
+    this.setState({
+      menuItems
+    });
+  }
+
   _renderSubMenu(item) {
     return (
       <List>
@@ -42,12 +65,13 @@ export default class MenuDrawer extends Component {
           className={ style['sub-menu-parent'] }
           key={ item.label }
           caption={ item.label }
-          onClick={ () => {} }
+          onClick={ this._toggleMenu.bind(this, item.id) }
         />
         {
+          item.open ?
           item.children.map((subMenuItem) => {
             return this._renderSubMenuItem(subMenuItem);
-          })
+          }) : null
         }
       </List>
 
@@ -72,7 +96,7 @@ export default class MenuDrawer extends Component {
         onOverlayClick={ this.props.onOverlayClick }
       >
         <div className={ style['menu'] }>
-          { this._renderNavigationItems(AdminNavigationLinks) }
+          { this._renderNavigationItems(this.state.menuItems) }
         </div>
       </Drawer>
     );
