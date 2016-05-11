0d2e3ee4de4044f528e9ed1248fc20a223463c94
Dariusz Paluch <dariusz.paluch@hotmail.com>
2016-05-11 11:29:54 +0200

    Add switch user Type to change profile data action

--- front/src/scripts/actions/change_profile_data.js
+++ front/src/scripts/actions/change_profile_data.js
@@ -2,7 +2,20 @@ import { fetchData } from './fetchData';
 import Qs from 'qs';
 
 export function changeProfileData(parameters, id, type) {
-  let userType = type === 'doctor' ? 'doctors' : 'patients';
+  let userType = '';
+
+  switch(type) {
+    case 'doctor':
+      userType = 'doctors';
+      break;
+    case 'patient':
+      userType = 'patients';
+      break;
+    case 'admin':
+      userType = 'admins';
+      break;
+  }
+
   let url = `/${ userType }/${ id }`;
   let body = JSON.stringify(
     parameters
