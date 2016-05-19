4777b43cf53030770adad0beee7ddb60da4ea817
Tomasz Tomys <tomasz@tomys.pl>
2016-05-19 23:53:45 +0200

    Add tests for Admins.

---
+++ api/tests/Calls/AdminsTest.php
@@ -0,0 +1,446 @@
+<?php
+class AdminsTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testMakePatient()
+    {
+        $mock = $this->getMockBuilder('\Database\Database')
+            ->getMock();
+
+        // Arrange
+        $patients = new \Calls\Patients($mock);
+
+        $obj = (object) [
+            'id' => 1,
+            'name' => 'Tomasz',
+            'surname' => 'Tomys',
+            'email' => 'tomasz@tomys.pl',
+            'type' => 'patient',
+            'pesel' => '12345678900'
+        ];
+
+        $array = $patients->_makePatient($obj);
+
+        // Assert
+        $this->assertEquals((array)$obj, $array);
+    }
+
+    public function testMakePatient2()
+    {
+        $mock = $this->getMockBuilder('\Database\Database')
+            ->getMock();
+
+        // Arrange
+        $patients = new \Calls\Patients($mock);
+
+        $obj = (object) [
+            'id' => 1,
+            'name' => 'Tomasz',
+            'surname' => 'Tomys',
+            'email' => 'tomasz@tomys.pl',
+            'password' => '123456',
+            'type' => 'patient',
+            'pesel' => '12345678900'
+        ];
+
+        $expObj = [
+            'id' => 1,
+            'name' => 'Tomasz',
+            'surname' => 'Tomys',
+            'email' => 'tomasz@tomys.pl',
+            'type' => 'patient',
+            'pesel' => '12345678900'
+        ];
+
+        $array = $patients->_makePatient($obj);
+
+        // Assert
+        $this->assertEquals($expObj, $array);
+    }
+
+
+    /**
+     * @expectedException \Exception
+     * @expectedExceptionMessage Some of required values not passed
+     */
+    public function testMakePatient3()
+    {
+        $mock = $this->getMockBuilder('\Database\Database')
+            ->getMock();
+
+        // Arrange
+        $patients = new \Calls\Patients($mock);
+
+        $obj = (object) [];
+
+        $patients->_makePatient($obj);
+    }
+
+
+    public function testIfPatientFound()
+    {
+        $mock = $this->getMockBuilder('\Database\Database')
+            ->getMock();
+
+        // Arrange
+        $patients = new \Calls\Patients($mock);
+
+        $result = $patients->_ifFoundPatient(1, 'patient');
+
+        // Assert
+        $this->assertTrue($result);
+    }
+
+    public function testIfPatientFound2()
+    {
+        $mock = $this->getMockBuilder('\Database\Database')
+            ->getMock();
+
+        // Arrange
+        $patients = new \Calls\Patients($mock);
+
+        $result = $patients->_ifFoundPatient(0, 'doctor');
+
+        // Assert
+        $this->assertFalse($result);
+    }
+
+    public function testIfPatientFound3()
+    {
+        $mock = $this->getMockBuilder('\Database\Database')
+            ->getMock();
+
+        // Arrange
+        $patients = new \Calls\Patients($mock);
+
+        $result = $patients->_ifFoundPatient(0, 'patient');
+
+        // Assert
+        $this->assertFalse($result);
+    }
+
+    public function testIfPatientFound4()
+    {
+        $mock = $this->getMockBuilder('\Database\Database')
+            ->getMock();
+
+        // Arrange
+        $patients = new \Calls\Patients($mock);
+
+        $result = $patients->_ifFoundPatient(1, 'doctor');
+
+        // Assert
+        $this->assertFalse($result);
+    }
+
+    public function testGetAllPatients() {
+        $mock = $this->getMockBuilder('\Database\Database')
+            ->getMock();
+
+        $array = [ (object)[
+            'id' => 1,
+            'name' => 'Tomasz',
+            'surname' => 'Tomys',
+            'email' => 'tomasz@tomys.pl',
+            'password' => '123456',
+            'type' => 'patient',
+            'pesel' => '12345678900'
+        ]];
+
+        $mock->expects($this->once())
+            ->method('findAllPatients')
+            ->will(
+                $this->returnValue($array)
+            );
+
+        $patients = new \Calls\Patients($mock);
+
+        $array = $patients->_getAllPatients();
+        $arrayExp = [[
+            'id' => 1,
+            'name' => 'Tomasz',
+            'surname' => 'Tomys',
+            'email' => 'tomasz@tomys.pl',
+            'type' => 'patient',
+            'pesel' => '12345678900'
+        ]];
+
+        $this->assertEquals($arrayExp, $array);
+    }
+
+    public function testGetAllPatients2() {
+        $mock = $this->getMockBuilder('\Database\Database')
+            ->getMock();
+
+        $array = [
+            (object)[
+                'id' => 1,
+                'name' => 'Tomasz',
+                'surname' => 'Tomys',
+                'email' => 'tomasz@tomys.pl',
+                'password' => '123456',
+                'type' => 'patient',
+                'pesel' => '12345678900'
+            ],
+            (object)[
+                'id' => 2,
+                'name' => 'Dariusz',
+                'surname' => 'Paluch',
+                'email' => 'dariusz@paluch.pl',
+                'password' => '12345622',
+                'type' => 'patient',
+                'pesel' => '12345678911'
+            ]
+        ];
+
+        $mock->expects($this->once())
+            ->method('findAllPatients')
+            ->will(
+                $this->returnValue($array)
+            );
+
+        $patients = new \Calls\Patients($mock);
+
+        $array = $patients->_getAllPatients();
+        $arrayExp = [
+            [
+                'id' => 1,
+                'name' => 'Tomasz',
+                'surname' => 'Tomys',
+                'email' => 'tomasz@tomys.pl',
+                'type' => 'patient',
+                'pesel' => '12345678900'
+            ],
+            [
+                'id' => 2,
+                'name' => 'Dariusz',
+                'surname' => 'Paluch',
+                'email' => 'dariusz@paluch.pl',
+                'type' => 'patient',
+                'pesel' => '12345678911'
+            ]
+        ];
+
+        $this->assertEquals($arrayExp, $array);
+    }
+
+    public function testGetAllPatients3() {
+        $mock = $this->getMockBuilder('\Database\Database')
+            ->getMock();
+
+        $array = [];
+
+        $mock->expects($this->once())
+            ->method('findAllPatients')
+            ->will(
+                $this->returnValue($array)
+            );
+
+        $patients = new \Calls\Patients($mock);
+
+        $array = $patients->_getAllPatients();
+        $arrayExp = [];
+
+        $this->assertEquals($arrayExp, $array);
+    }
+
+    /**
+     * @expectedException \Exception
+     * @expectedExceptionMessage It's not a patient
+     */
+    public function testGetAllPatients4() {
+        $mock = $this->getMockBuilder('\Database\Database')
+            ->getMock();
+
+        $array = [
+            (object)[
+                'id' => 1,
+                'name' => 'Tomasz',
+                'surname' => 'Tomys',
+                'email' => 'tomasz@tomys.pl',
+                'password' => '123456',
+                'type' => 'doctor',
+                'specialization' => 'surgeon'
+            ],
+            (object)[
+                'id' => 2,
+                'name' => 'Dariusz',
+                'surname' => 'Paluch',
+                'email' => 'dariusz@paluch.pl',
+                'password' => '12345622',
+                'type' => 'patient',
+                'pesel' => '11123232323'
+            ]
+        ];
+
+        $mock->expects($this->once())
+            ->method('findAllPatients')
+            ->will(
+                $this->returnValue($array)
+            );
+
+        $patients = new \Calls\Patients($mock);
+
+        $patients->_getAllPatients();
+    }
+
+    public function testDeletePatient() {
+        $mock = $this->getMockBuilder('\Database\Database')
+            ->getMock();
+
+        $array = [
+            (object)[
+                'id' => 1,
+                'name' => 'Tomasz',
+                'surname' => 'Tomys',
+                'email' => 'tomasz@tomys.pl',
+                'password' => '123456',
+                'type' => 'patient',
+                'pesel' => '12345555555'
+            ],
+            (object)[
+                'id' => 2,
+                'name' => 'Dariusz',
+                'surname' => 'Paluch',
+                'email' => 'dariusz@paluch.pl',
+                'password' => '12345622',
+                'type' => 'patient',
+                'pesel' => '11123232323'
+            ]
+        ];
+
+        $array2 = [
+            (object)[
+                'id' => 2,
+                'name' => 'Dariusz',
+                'surname' => 'Paluch',
+                'email' => 'dariusz@paluch.pl',
+                'password' => '12345622',
+                'type' => 'patient',
+                'pesel' => '11123232323'
+            ]
+        ];
+
+        $expArray = [
+            [
+                'id' => 1,
+                'name' => 'Tomasz',
+                'surname' => 'Tomys',
+                'email' => 'tomasz@tomys.pl',
+                'type' => 'patient',
+                'pesel' => '12345555555'
+            ],
+            [
+                'id' => 2,
+                'name' => 'Dariusz',
+                'surname' => 'Paluch',
+                'email' => 'dariusz@paluch.pl',
+                'type' => 'patient',
+                'pesel' => '11123232323'
+            ]
+        ];
+
+        $expArray2 = [
+            [
+                'id' => 2,
+                'name' => 'Dariusz',
+                'surname' => 'Paluch',
+                'email' => 'dariusz@paluch.pl',
+                'type' => 'patient',
+                'pesel' => '11123232323'
+            ]
+        ];
+
+        $mock->expects($this->at(0))
+            ->method('findAllPatients')
+            ->will(
+                $this->returnValue($array)
+            );
+
+        $mock->expects($this->once())
+            ->method('loadUserById')
+            ->withAnyParameters()
+            ->will(
+                $this->returnValue($array[0])
+            );
+
+        $mock->expects($this->once())
+            ->method('trash');
+
+        $patients = new \Calls\Patients($mock);
+        $this->assertEquals($expArray, $patients->_getAllPatients());
+
+
+        $patients->trashPatient(1);
+
+        $mock->expects($this->at(0))
+            ->method('findAllPatients')
+            ->will(
+                $this->returnValue($array2)
+            );
+
+        $this->assertEquals($expArray2, $patients->_getAllPatients());
+    }
+
+    public function testDeletePatient2() {
+        $mock = $this->getMockBuilder('\Database\Database')
+            ->getMock();
+
+        $array = [
+            (object)[
+                'id' => 1,
+                'name' => 'Tomasz',
+                'surname' => 'Tomys',
+                'email' => 'tomasz@tomys.pl',
+                'password' => '123456',
+                'type' => 'patient',
+                'pesel' => '12345555555'
+            ]
+        ];
+
+        $array2 = [];
+
+        $expArray = [
+            [
+                'id' => 1,
+                'name' => 'Tomasz',
+                'surname' => 'Tomys',
+                'email' => 'tomasz@tomys.pl',
+                'type' => 'patient',
+                'pesel' => '12345555555'
+            ]
+        ];
+
+        $expArray2 = [];
+
+        $mock->expects($this->at(0))
+            ->method('findAllPatients')
+            ->will(
+                $this->returnValue($array)
+            );
+
+        $mock->expects($this->once())
+            ->method('loadUserById')
+            ->withAnyParameters()
+            ->will(
+                $this->returnValue($array[0])
+            );
+
+        $mock->expects($this->once())
+            ->method('trash');
+
+        $patients = new \Calls\Patients($mock);
+        $this->assertEquals($expArray, $patients->_getAllPatients());
+
+
+        $patients->trashPatient(1);
+
+        $mock->expects($this->at(0))
+            ->method('findAllPatients')
+            ->will(
+                $this->returnValue($array2)
+            );
+
+        $this->assertEquals($expArray2, $patients->_getAllPatients());
+    }
+}
